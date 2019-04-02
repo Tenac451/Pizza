fa1fad49a0416fb6c41c21272dd32c54f07ff13d
Jan Münchberger <muenchj@gfz-potsdam.de>
2019-04-02 20:11:22 +0200

    uebung 2

--- src/CustomerVO.java
+++ src/CustomerVO.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -15,8 +16,13 @@ public class CustomerVO {
 	
 	private String gender;
 	
+	private static int nextId = 0;
+	
+	private int id;
+	
 	static final String M = "männlich";
 	static final String W = "weiblich";
+	static final String N = "";
 	static final String D = "divers";
 	
 	private LocalDate dateOfBirth;
@@ -27,29 +33,77 @@ public class CustomerVO {
 		this.setFirstName(firstName);
 		this.setGender(gender);
 		this.setDateOfBirth(dateOfBirth);
+		this.id = CustomerVO.nextId;
+		CustomerVO.nextId = CustomerVO.nextId + 1;
 	}
 	
 	public CustomerVO(String lastName, String firstName, LocalDate dateOfBirth) {
-		super();
-		this.setLastName(lastName);
-		this.setFirstName(firstName);
-		this.setDateOfBirth(dateOfBirth);
+		this(lastName , firstName, CustomerVO.N , dateOfBirth);
 	}
 	
 	public CustomerVO() {
 		this("Mampf", "Martin", LocalDate.of(1990, 5, 24));
 	}
 	
-	
-	public String dobToString() {
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CustomerVO other = (CustomerVO) obj;
+		if (dateOfBirth == null) {
+			if (other.dateOfBirth != null)
+				return false;
+		} else if (!dateOfBirth.equals(other.dateOfBirth))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+
+	private String dobToString() {
 		String result = "";
-		
-		//result = this.getDateOfBirth().toString();
 		result = this.getDateOfBirth().format(DateTimeFormatter.ofPattern("dd MM yyyy"));
 		return result;
 	}
+	
+	public short calculateAge() {
+		short age = 0;
+		int diff = Period.between(this.getDateOfBirth(), LocalDate.now()).getYears();
+		if(diff < 32767) {
+			age = (short) diff;
+		}
+		return age;
+	}
 	public String toString() {
-		return this.getFirstName() + ' ' + this.getLastName();
+		return "ID: "+ this.getId() + ' ' + this.getFirstName() + ' ' + this.getLastName() + ' ' + this.dobToString() + " ist somit " + calculateAge();
 	}
 	
 	public String getLastName() {
@@ -77,11 +131,29 @@ public class CustomerVO {
 	}
 	
 	public LocalDate getDateOfBirth() {
-		return dateOfBirth;
+		if(dateOfBirth != null) {
+			return dateOfBirth;
+		} else {
+			return LocalDate.now();
+		}
+
 	}
 	
 	public void setDateOfBirth(LocalDate dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
+		int diff = Period.between(dateOfBirth, LocalDate.now()).getYears();
+		if(diff > 17) {
+			this.dateOfBirth = dateOfBirth;
+		} else {
+			this.dateOfBirth = null;
+		}
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public static int getNextId() {
+		return nextId;
 	}
 	
 	
--- src/PizzaVO.java
+++ src/PizzaVO.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 
  * @author Jan Münchberger
@@ -22,6 +24,47 @@ public class PizzaVO {
 
 	public PizzaVO() {
 		super();
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(ingredients);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + Float.floatToIntBits(price);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PizzaVO other = (PizzaVO) obj;
+		if (!Arrays.equals(ingredients, other.ingredients))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Float.floatToIntBits(price) != Float.floatToIntBits(other.price))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public Object clone() {
+		PizzaVO cloneObj = new PizzaVO(this.getName(),this.getIngredients(),this.getPrice());
+		return cloneObj;	
+	}
+	
+	public String toString() {
+		return this.getName();
 	}
 
 	public String[] getIngredients() {
